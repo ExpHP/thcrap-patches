53bb64155cb35fdf9152fb709574987229a81a98
Michael Lamparski <diagonaldevice@gmail.com>
2020-05-30 15:02:08 -0400

    missed one

--- patches/c_key/patch.js
+++ patches/c_key/patch.js
@@ -6,5 +6,5 @@
 	"servers": [
 		"https://raw.githubusercontent.com/ExpHP/thcrap-patches/master/patches/c_key/"
 	],
-	"title": "(TH17) Make Ctrl and C behave as separate keys internally, for use by other patches. (C maps to 0x4)"
+	"title": "(17) Make Ctrl and C behave as separate keys internally, for use by other patches. (C maps to 0x4)"
 }
--- patches/repo.js
+++ patches/repo.js
@@ -2,7 +2,7 @@
 	"contact": "diagonaldevice@gmail.com",
 	"id": "ExpHP",
 	"patches": {
-		"c_key": "(TH17) Make Ctrl and C behave as separate keys internally, for use by other patches. (C maps to 0x4)",
+		"c_key": "(17) Make Ctrl and C behave as separate keys internally, for use by other patches. (C maps to 0x4)",
 		"nopause_enm": "(11-17, 125) a.k.a. the Za Warudo mod.  Enemies don't stop when you pause.  nopauseblur recommended where available.",
 		"nopauseblur": "(15-17, 165) Disables blur on pause.  Not available yet for DDC and earlier due to technical difficulties.",
 		"sp_resources": "(13, 14, 16, 17) Spell practice with lives, bombs and Max Season",
