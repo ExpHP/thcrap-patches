a4765f9e4a9060eeaee306148b541ba865264d30
Michael Lamparski <diagonaldevice@gmail.com>
2020-05-31 12:43:37 -0400

    one more

--- patches/c_key/patch.js
+++ patches/c_key/patch.js
@@ -6,5 +6,5 @@
 	"servers": [
 		"https://raw.githubusercontent.com/ExpHP/thcrap-patches/master/patches/c_key/"
 	],
-	"title": "(17) Make Ctrl and C behave as separate keys internally, for use by other patches. (C maps to 0x4)"
+	"title": "(TH17) Make Ctrl and C behave as separate keys internally, for use by other patches. (C maps to 0x4)"
 }
