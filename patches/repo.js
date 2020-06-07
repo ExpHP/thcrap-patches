70345c1e585a66000b9a8306730396b3ef872789
Michael Lamparski <diagonaldevice@gmail.com>
2020-06-07 14:53:24 -0400

    run update.sh

--- patches/repo.js
+++ patches/repo.js
@@ -3,7 +3,7 @@
 	"id": "ExpHP",
 	"patches": {
 		"c_key": "(17) Make Ctrl and C behave as separate keys internally, for use by other patches. (C maps to 0x4)",
-		"continue": "(10, 12) Fixes the continue system. SA support coming soon. plz don't save cursed replays kthx",
+		"continue": "(10-12) Fixes the continue system. plz don't save cursed 3cc replays kthx",
 		"nopause_enm": "(11-17, 125) a.k.a. the Za Warudo mod.  Enemies don't stop when you pause.  nopauseblur recommended where available.",
 		"nopauseblur": "(15-17, 165) Disables blur on pause.  Not available yet for DDC and earlier due to technical difficulties.",
 		"sp_resources": "(13, 14, 16, 17) Spell practice with lives, bombs and Max Season",
