c4083fc0c560ba4fc7f53c9b0ef89c94001c13f3
Michael Lamparski <diagonaldevice@gmail.com>
2020-10-04 12:44:24 -0400

    actually publish base_exphp, oops

---
+++ patches/base_exphp/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "base_exphp",
+	"servers": [
+		"https://raw.githubusercontent.com/ExpHP/thcrap-patches/master/patches/base_exphp/"
+	],
+	"title": "Provides functions that help other patches support patches like bullet_cap without directly depending on them."
+}
