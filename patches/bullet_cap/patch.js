47f9366996b4356cec616f07c7f8ff3dd8a83ded
Michael Lamparski <diagonaldevice@gmail.com>
2020-10-16 16:53:13 -0400

    call update.sh

--- patches/bullet_cap/patch.js
+++ patches/bullet_cap/patch.js
@@ -9,5 +9,5 @@
 	"servers": [
 		"https://raw.githubusercontent.com/ExpHP/thcrap-patches/master/patches/bullet_cap/"
 	],
-	"title": "(08 (in beta), 10-13, 125, 128) Makes the bullet cap, laser cap, and cancel cap configurable.\nDefaults to 16x everything. To configure, see http://exphp.github.io/thpages/#/mods/bullet-cap"
+	"title": "(08 (in beta), 10-13, 15-17, 125, 128, 165) Makes the bullet cap, laser cap, and cancel cap configurable.\nDefaults to 16x everything. To configure, see http://exphp.github.io/thpages/#/mods/bullet-cap"
 }
--- patches/sprite_death_fix/patch.js
+++ patches/sprite_death_fix/patch.js
@@ -6,5 +6,5 @@
 	"servers": [
 		"https://raw.githubusercontent.com/ExpHP/thcrap-patches/master/patches/sprite_death_fix/"
 	],
-	"title": "(08, 10-13, 125, 128) Fixes crashes from drawing too many sprites."
+	"title": "(08, 10-17 STGs) Fixes crashes and black screens from drawing too many sprites."
 }
