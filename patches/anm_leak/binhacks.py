83e8e0ff314170f120062f6ab2a982799f1902ae
Michael Lamparski <diagonaldevice@gmail.com>
2021-05-16 22:01:20 -0400

    update anm_leak for older games to fix crashes

--- patches/anm_leak/binhacks.py
+++ patches/anm_leak/binhacks.py
@@ -12,9 +12,10 @@ def main():
     thc = binhack_helper.ThcrapGen('ExpHP.anm-buffers.')
     defs = binhack_helper.NasmDefs.from_file_rel('common.asm')
 
-    if game == 'th17':  # FIXME:  migrate other games to new binhacks
+    if 'th15' <= game:
         add_main_binhacks(game, thc, defs)
-        add_th18_perf_fixes(game, thc, defs)
+    if 'th17' <= game <= 'th18':
+        add_th17_perf_fixes(game, thc, defs)
 
     thc.print()
 
@@ -23,10 +24,10 @@ def add_main_binhacks(game, thc: binhack_helper.ThcrapGen, defs):
     game_data = {
         'th15':  defs.GameData(vm_size=0x608, id_offset=0x544, func_malloc=0x49039f),
         'th16':  defs.GameData(vm_size=0x5fc, id_offset=0x538, func_malloc=0x4749ac),
-        'th165':  defs.GameData(vm_size=0x5fc, id_offset=0x538, func_malloc=0x47a78d),
-        'th17':   defs.GameData(vm_size=0x600, id_offset=0x538, func_malloc=0x47b250),
+        'th165': defs.GameData(vm_size=0x5fc, id_offset=0x538, func_malloc=0x47a78d),
+        'th17':  defs.GameData(vm_size=0x600, id_offset=0x538, func_malloc=0x47b250),
         'th18.v0.02a': defs.GameData(vm_size=0x60c, id_offset=0x544, func_malloc=0x484851),
-        'th18':   defs.GameData(vm_size=0x60c, id_offset=0x544, func_malloc=0x48dc71),
+        'th18': defs.GameData(vm_size=0x60c, id_offset=0x544, func_malloc=0x48dc71),
     }[game]
 
     thc.codecave('game-data', thc.data(game_data))
@@ -45,12 +46,14 @@ def add_main_binhacks(game, thc: binhack_helper.ThcrapGen, defs):
         })
     # replace the "push, call, stack cleanup" sequence
     if game=='th15':   hook_alloc(0x48954f, jmp_addr=0x48955c, expected='6808060000')
-    if game=='th16':   hook_alloc(0x46f6ef, jmp_addr=0x46f6fc, expected='68fc050000')
-    if game=='th165':  hook_alloc(0x475a1f, jmp_addr=0x475a2c, expected='68fc050000')
+    elif game=='th16':   hook_alloc(0x46f6ef, jmp_addr=0x46f6fc, expected='68fc050000')
+    elif game=='th165':  hook_alloc(0x475a1f, jmp_addr=0x475a2c, expected='68fc050000')
     # these games defer the stack cleanup so only replace the call
-    if game=='th17':   hook_alloc(0x476b54, jmp_addr=0x476b59, expected='e8f7460000')
-    # if game=='th18.v0.02a': hook_alloc(0x47fbd4, jmp_addr=0x47fbd9, expected='e8784c0000')
-    if game=='th18':   hook_alloc(0x489414, jmp_addr=0x489419, expected='e858480000')
+    elif game=='th17':   hook_alloc(0x476b54, jmp_addr=0x476b59, expected='e8f7460000')
+    # elif game=='th18.v0.02a': hook_alloc(0x47fbd4, jmp_addr=0x47fbd9, expected='e8784c0000')
+    elif game=='th18.v1.00a':   hook_alloc(0x489414, jmp_addr=0x489419, expected='e858480000')
+    else:
+        assert False, game
 
     def hook_dealloc(binhack_addr, vm_reg, expected):
         thc.binhack('dealloc', {
@@ -66,11 +69,13 @@ def add_main_binhacks(game, thc: binhack_helper.ThcrapGen, defs):
         })
     # Replace the call to the C++ stdlib 'operator delete'
     if game == 'th15':   hook_dealloc(0x44c97c, vm_reg='esi', expected='e86f3a0400')
-    if game == 'th16':   hook_dealloc(0x43b941, vm_reg='esi', expected='e899900300')
-    if game == 'th165':  hook_dealloc(0x438bfe, vm_reg='esi', expected='e85c570400')
-    if game == 'th17':   hook_dealloc(0x476083, vm_reg='esi', expected='e8f8510000')
-    # if game == 'th18.v0.02a': hook_dealloc(0x47f0c3, vm_reg='esi', expected='e8b9570000')
-    if game == 'th18':   hook_dealloc(0x488753, vm_reg='esi', expected='e849550000')
+    elif game == 'th16':   hook_dealloc(0x43b941, vm_reg='esi', expected='e899900300')
+    elif game == 'th165':  hook_dealloc(0x438c31, vm_reg='esi', expected='e8871b0400')
+    elif game == 'th17':   hook_dealloc(0x476083, vm_reg='esi', expected='e8f8510000')
+    # elif game == 'th18.v0.02a': hook_dealloc(0x47f0c3, vm_reg='esi', expected='e8b9570000')
+    elif game == 'th18.v1.00a':   hook_dealloc(0x488753, vm_reg='esi', expected='e849550000')
+    else:
+        assert False, game
 
     # ===================================
 
@@ -79,12 +84,14 @@ def add_main_binhacks(game, thc: binhack_helper.ThcrapGen, defs):
             'addr': binhack_addr,
             'code': thc.asm(lambda c: f''' jmp {c.rel_auto('new-search')} '''),
         })
-    # Hook the line that reads world_list_head.
-    # Jump to function cleanup.
-    # if game == 'th15':   hook_search(0x488534, id_reg='eax', jmp_addr=0x488573, expected='8b96dc000000')
-    # if game == 'th16':   hook_search(0x46efc4, id_reg='eax', jmp_addr=0x46f003, expected='8b96dc000000')
-    # if game == 'th165':  hook_search(0x47530d, id_reg='eax', jmp_addr=0x47535a, expected='8b96dc000000')
-    if game == 'th17':   hook_search(0x476470)
+    # Replace the whole function.
+    if game == 'th15':    hook_search(0x488510)
+    elif game == 'th16':  hook_search(0x46efa0)
+    elif game == 'th165': hook_search(0x4752f0)
+    elif game == 'th17':  hook_search(0x476470)
+    elif game == 'th18.v1.00a':  hook_search(0x488b40)
+    else:
+        assert False, game
     # if game == 'th18.v0.02a': hook_search(0x47f49d, id_reg='eax', jmp_addr=0x47f4ea, expected='8b8ef0060000')
     # if game == 'th18':   hook_search(0x488b5d, id_reg='eax', jmp_addr=0x488baa, expected='8b8ef0060000')
 
@@ -102,7 +109,7 @@ def add_main_binhacks(game, thc: binhack_helper.ThcrapGen, defs):
     elif game == 'th17': disable_fast_array(0x476a79, 0x476b4f)
     # These games REQUIRE it because they use our layer-drawing optimizations and we need to make sure
     # the number of existing batches is adequate for the layer array.
-    elif game == 'th18': disable_fast_array(0x489339, 0x48940f)
+    elif game == 'th18.v1.00a': disable_fast_array(0x489339, 0x48940f)
     else:
         assert False, game
 
@@ -123,19 +130,37 @@ def add_main_binhacks(game, thc: binhack_helper.ThcrapGen, defs):
             ret
         '''),
     })
-    if game == 'th17':
+    if game == 'th15':
+        hack_id.at(0x487c72, vm_reg='edi', id_reg='ecx')
+        hack_id.at(0x487d2c, vm_reg='edi', id_reg='ecx')
+        hack_id.at(0x487de2, vm_reg='edi', id_reg='ecx')
+        hack_id.at(0x487e9c, vm_reg='edi', id_reg='ecx')
+    elif game == 'th16':
+        hack_id.at(0x46e872, vm_reg='edi', id_reg='ecx')
+        hack_id.at(0x46e92c, vm_reg='edi', id_reg='ecx')
+        hack_id.at(0x46e9e2, vm_reg='edi', id_reg='ecx')
+        hack_id.at(0x46ea9c, vm_reg='edi', id_reg='ecx')
+    elif game == 'th165':
+        hack_id.at(0x474a7d, vm_reg='edi', id_reg='ecx')
+        hack_id.at(0x474b0e, vm_reg='edx', id_reg='ecx')
+        hack_id.at(0x474bbd, vm_reg='edi', id_reg='ecx')
+        hack_id.at(0x474c4e, vm_reg='edx', id_reg='ecx')
+    elif game == 'th17':
         hack_id.at(0x475d18, vm_reg='edi', id_reg='ecx')
         hack_id.at(0x475da9, vm_reg='edx', id_reg='ecx')
         hack_id.at(0x475e58, vm_reg='edi', id_reg='ecx')
         hack_id.at(0x475ee9, vm_reg='edx', id_reg='ecx')
+    elif game == 'th18':
+        hack_id.at(0x4883e8, vm_reg='edi', id_reg='ecx')
+        hack_id.at(0x488479, vm_reg='edx', id_reg='ecx')
+        hack_id.at(0x488528, vm_reg='edi', id_reg='ecx')
+        hack_id.at(0x4885b9, vm_reg='edx', id_reg='ecx')
     else:
         assert False, game
 
 
-def add_th18_perf_fixes(game, thc: binhack_helper.ThcrapGen, defs):
+def add_th17_perf_fixes(game, thc: binhack_helper.ThcrapGen, defs):
     thc.codecave('layer-data', thc.data({
-        'th15': 0, 'th16': 0, 'th165': 0,
-        'th18.v0.02a': 0,
         'th17': defs.GameLayerData(
             func_draw_vm=0x46f8c0,
             world_list_offset=0x6dc,
@@ -184,7 +209,9 @@ def add_th18_perf_fixes(game, thc: binhack_helper.ThcrapGen, defs):
     # so I chose a spot inside UpdateFuncRegistry::run_all_on_draw.
     # (AFTER aquiring the critical section)
     if game == 'th17': build_layer_array(0x401409, jmp_addr=0x40140f, anm_manager=0x509a20, expected='8b4840 53 33db')
-    if game == 'th18': build_layer_array(0x401449, jmp_addr=0x40144f, anm_manager=0x51f65c, expected='8b4840 53 33db')
+    elif game == 'th18': build_layer_array(0x401449, jmp_addr=0x40144f, anm_manager=0x51f65c, expected='8b4840 53 33db')
+    else:
+        assert False, game
 
     def optimize_draw_layer(binhack_addr, layer, anm_mgr_reg, jmp_addr, expected):
         thc.binhack('optimized-draw-layer', {
@@ -199,33 +226,10 @@ def add_th18_perf_fixes(game, thc: binhack_helper.ThcrapGen, defs):
         })
     # Replace the entire part of the function inside the critical section.
     if game == 'th17': optimize_draw_layer(0x475bc2, layer='ebp+0x8', anm_mgr_reg='esi', jmp_addr=0x475c58, expected="8b86dc060000")
-    if game == 'th18': optimize_draw_layer(0x488282, layer='ebp+0x8', anm_mgr_reg='esi', jmp_addr=0x488328, expected="8b86f0060000")
+    elif game == 'th18': optimize_draw_layer(0x488282, layer='ebp+0x8', anm_mgr_reg='esi', jmp_addr=0x488328, expected="8b86f0060000")
+    else:
+        assert False, game
 
-    # ---------
-    # One last little hack:
-    #
-    # Because we were forced to disable the vanilla fast path for VMs, we actually deoptimized AnmManager::get_vm_with_id.
-    # A LOT.  Which is bad because EffectManager::on_tick calls it. A LOT.  Enough to slow us down to 20FPS.
-    #
-    # But here's the funny thing:
-    #
-    # As far as I can tell, these calls in EffectManager::on_tick currently serve NO PURPOSE.
-    # The array and these calls were added in LoLK.  To my understanding, having reversed the relevant parts of TH15 and TH16,
-    # all it's doing is keeping track of which effects are still alive, so that those VMs can be duplicated when a PointDevice
-    # snapshot is created.  Which will obviously never happen in TH18.
-    #
-    # So... we'll just make it unconditionally believe that all effect VMs are dead, and it shouldn't be a problem.
-    def no_effect_manager(binhack_addr, expected):
-        thc.binhack('forgetful-effect-manager', {
-            'addr': binhack_addr,
-            'expected': expected,
-            'code': thc.asm(lambda c: f'''
-                pop eax  # balance the stack
-                xor eax, eax
-                nop; nop
-            '''),
-        })
-    if game == 'th18': no_effect_manager(0x42af45, expected="e8f6db0500")
 
 if __name__ == '__main__':
     main()
