f8b4bd38c0cba9bd7cea236315427937badbf370
Michael Lamparski <diagonaldevice@gmail.com>
2022-09-20 15:57:46 -0400

    mention TH18 in debug_counters patch.js, oops

--- patches/debug_counters/patch.js
+++ patches/debug_counters/patch.js
@@ -7,5 +7,5 @@
 	"servers": [
 		"https://raw.githubusercontent.com/ExpHP/thcrap-patches/master/patches/debug_counters/"
 	],
-	"title": "(06-17 STGs) Show sprite, bullet, laser, enemy, and item counts."
+	"title": "(06-18 STGs) Show sprite, bullet, laser, enemy, and item counts."
 }
